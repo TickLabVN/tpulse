cc3c102721b5764ae9032c61af52253ddbe0ecdc
phucvinh57 <npvinh0507>
2023-11-23 14:56:19 +0700

    feat: detect mouse and key press to determine user AFK or not

--- src/afk/watcher.rs
+++ src/afk/watcher.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use device_query::{DeviceQuery, DeviceState};
 use log::info;
 use std::thread::sleep;
@@ -5,16 +6,13 @@ use std::time::Duration;
 
 #[derive(Clone)]
 pub struct Settings {
+    /// In milisecs
     timeout: u64,
+    /// In milisecs
     poll_time: u64,
 }
 impl Settings {
     pub fn new(timeout: u64, poll_time: u64) -> Self {
-        assert!(
-            timeout >= poll_time,
-            "Timeout should be greater than or equal to poll time"
-        );
-
         Settings { timeout, poll_time }
     }
 }
@@ -23,6 +21,12 @@ pub struct AFKWatcher {
     settings: Settings,
     bucketname: String,
 }
+
+enum AFKState {
+    ONLINE,
+    OFFLINE,
+}
+
 impl AFKWatcher {
     pub fn new(settings: &Settings) -> Self {
         AFKWatcher {
@@ -32,18 +36,59 @@ impl AFKWatcher {
     }
     pub fn run(&self) {
         info!("AFK watcher started");
-        self.watch();
-    }
-    fn watch(&self) {
-        // let mut afk = false;
         let device_state = DeviceState::new();
-        loop {
-            sleep(Duration::from_secs(self.settings.poll_time));
+        let mut mouse_pos = device_state.get_mouse().coords;
 
-            let keys = device_state.get_keys();
-            info!("Detected key {:?}", keys.len() > 0);
-            let mouse = device_state.get_mouse();
-            info!("Current mouse position {:?}", mouse);
+        let mut timeout = 0;
+        let mut afk = false;
+        loop {
+            sleep(Duration::from_millis(self.settings.poll_time));
+
+            let mut detect_interact = false;
+
+            let current_mouse_pos = device_state.get_mouse().coords;
+            if current_mouse_pos.0 != mouse_pos.0 && current_mouse_pos.1 != mouse_pos.1 {
+                mouse_pos = current_mouse_pos;
+                info!("Detect mouse position change {:?}", mouse_pos);
+                detect_interact = true;
+            } else {
+                let keys = device_state.query_keymap();
+                if keys.len() > 0 {
+                    info!("Detected key {:?}", keys);
+                    detect_interact = true;
+                }
+            }
+
+            if detect_interact {
+                timeout = 0;
+                if afk {
+                    afk = false;
+                    self.send_metric(AFKState::ONLINE);
+                }
+            } else {
+                timeout += self.settings.poll_time;
+                if timeout >= self.settings.timeout && !afk {
+                    afk = true;
+                    self.send_metric(AFKState::OFFLINE);
+                }
+            }
+        }
+    }
+    fn send_metric(&self, state: AFKState) {
+        let now = Utc::now();
+        match state {
+            AFKState::ONLINE => {
+                info!("{}: Reconnected at {}", self.bucketname, now.to_string());
+            }
+            AFKState::OFFLINE => {
+                let afk_from = now - chrono::Duration::milliseconds(self.settings.timeout as i64);
+                info!(
+                    "{}: Noticed AFK from {} to {}",
+                    self.bucketname,
+                    afk_from.to_string(),
+                    now.to_string()
+                );
+            }
         }
     }
 }
