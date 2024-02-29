68a2ada4398c7e31187d69ac25bf60fed6c92d24
ngyngcphu <nguyenngocphu225@gmail.com>
2024-03-01 04:27:32 +0700

    chore(template): config Tailwind utility

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { BaseStyles, Box, Heading } from '@primer/react';
+import { Page } from '@pages';
 
 function App() {
-  return (
-    <BaseStyles>
-      <Box m={4}>
-        <Heading as='h2' sx={{ mb: 2 }}>
-          Hello, world!
-        </Heading>
-        <p>This will get Primer text styles.</p>
-      </Box>
-    </BaseStyles>
-  );
+  return <Page />;
 }
 
 export default App;
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,3 +1,14 @@
+import { PageLayout } from '@primer/react';
+
 export function Page() {
-  return <></>;
+  return (
+    <PageLayout>
+      <PageLayout.Header>
+        <p className='text-red-500'>Header</p>
+      </PageLayout.Header>
+      <PageLayout.Content>Content</PageLayout.Content>
+      <PageLayout.Pane>Pane</PageLayout.Pane>
+      <PageLayout.Footer>Footer</PageLayout.Footer>
+    </PageLayout>
+  );
 }
