24ad049548e9905845892407f73e32b0ef5c1e3a
ngyngcphu <nguyenngocphu225@gmail.com>
2024-03-01 01:49:20 +0700

    feat(template): integrate UI library into project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import viteEslint from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteEslint()],
+  plugins: [react(), viteEslint(), tsconfigPaths()],
   // prevent vite from obscuring rust errors
   clearScreen: false,
   // Tauri expects a fixed port, fail if that port is not available
