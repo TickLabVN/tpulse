4e2fa9f673505610f4eabe3b9b98d3d26b5e977d
quannhg <quan.nguyenhg12@gmail.com>
2024-03-07 11:30:10 +0700

    feat(setting): read setting from json file

---
+++ src-tauri/src/setting.rs
@@ -0,0 +1,84 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+struct AppConfig {
+    settings: HashMap<Setting, Option<String>>,
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub enum Setting {
+    PollTime,
+    Timeout,
+}
+
+impl AppConfig {
+    fn get_setting(&self, setting: &Setting) -> Option<&Option<String>> {
+        self.settings.get(setting)
+    }
+
+    fn set_setting<T>(&mut self, setting: Setting, new_value: Option<T>)
+    where
+        T: Into<String>,
+    {
+        self.settings.insert(setting, new_value.map(Into::into));
+    }
+}
+
+const CONFIG_FILE_PATH: &str = "setting.json";
+
+fn read_all_settings_from_file() -> Result<AppConfig, Box<dyn std::error::Error>> {
+    let file_contents = fs::read_to_string(CONFIG_FILE_PATH)?;
+    let config: AppConfig = serde_json::from_str(&file_contents)?;
+    Ok(config)
+}
+
+fn write_all_settings_to_file(config: &AppConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let json_config = serde_json::to_string_pretty(&config)?;
+    fs::write(CONFIG_FILE_PATH, json_config)?;
+    Ok(())
+}
+
+pub fn read_setting_from_file<T>(
+    setting_name: Setting,
+) -> Result<Option<T>, Box<dyn std::error::Error>>
+where
+    T: serde::de::DeserializeOwned,
+{
+    let settings = read_all_settings_from_file().unwrap_or_else(|err| {
+        eprintln!("Error reading configuration: {}", err);
+        AppConfig::default()
+    });
+
+    if let Some(setting_value) = settings.get_setting(&setting_name).cloned().flatten() {
+        let parsed_value: T = serde_json::from_str(&setting_value)?;
+        Ok(Some(parsed_value))
+    } else {
+        Ok(None)
+    }
+}
+
+pub fn write_setting_to_file(
+    setting_name: Setting,
+    new_value: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut settings = read_all_settings_from_file().unwrap_or_else(|err| {
+        eprintln!("Error reading configuration: {}", err);
+        AppConfig::default()
+    });
+    settings.set_setting(setting_name, Some(new_value));
+
+    write_all_settings_to_file(&settings)?;
+
+    Ok(())
+}
+
+pub fn handle_setting_error<T>(setting: Setting, err: &Box<dyn Error>, default_value: T) -> T
+where
+    T: Default,
+{
+    eprintln!("Error reading {:?} setting: {}", setting, err);
+    default_value
+}
