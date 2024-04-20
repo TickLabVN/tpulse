83d7e9cfe18a1a08cd92c7759a9743c55b0e975c
HuyDNA <huydo862003@gmail.com>
2024-04-20 13:05:43 +0700

    refactor: move some models into processed result

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -7,14 +7,3 @@ pub struct BrowserLogModel {
     pub start_time: String,
     pub title: Option<String>,
 }
-
-#[derive(Debug, Clone)]
-pub struct LogModel {
-    pub start_time: String,
-    pub activity_identifier: String,
-}
-
-pub struct UpdateEndTimeForLogModel {
-    pub start_time: String,
-    pub end_time: String,
-}
--- src-tauri/src/raw_metric_processor/mod.rs
+++ src-tauri/src/raw_metric_processor/mod.rs
@@ -2,15 +2,28 @@ pub mod processors;
 
 use std::{future::Future, pin::Pin};
 
-use crate::{metrics::UserMetric, models::LogModel};
+use crate::metrics::UserMetric;
+
+#[derive(Clone)]
+pub enum ProcessedResult {
+    StartActivity {
+        start_time: String,
+        activity_identifier: String,
+    },
+    UpdateEndActivity {
+        start_time: String,
+        end_time: String,
+    },
+}
 
 pub trait MetricProcessor {
-    fn process(&mut self, metric: &UserMetric) -> Option<LogModel>;
+    fn process(&mut self, metric: &UserMetric) -> Option<ProcessedResult>;
 }
 
 pub struct RawMetricProcessorManager {
     processor_list: Vec<Box<dyn MetricProcessor>>,
-    handler_list: Vec<Box<dyn Fn(LogModel) -> Pin<Box<dyn Future<Output = ()> + Send + Sync>>>>,
+    handler_list:
+        Vec<Box<dyn Fn(ProcessedResult) -> Pin<Box<dyn Future<Output = ()> + Send + Sync>>>>,
 }
 
 impl RawMetricProcessorManager {
@@ -19,24 +32,24 @@ impl RawMetricProcessorManager {
     }
     pub fn register_handler(
         &mut self,
-        handler: impl Fn(LogModel) -> Pin<Box<dyn Future<Output = ()> + Send + Sync>> + 'static,
+        handler: impl Fn(ProcessedResult) -> Pin<Box<dyn Future<Output = ()> + Send + Sync>> + 'static,
     ) {
         self.handler_list.push(Box::new(handler));
     }
 
     pub async fn handle_metric(mut self: Pin<&mut Self>, metric: UserMetric) {
-        let mut log_model = None;
+        let mut result = None;
         for processor in &mut self.processor_list {
             let res = processor.as_mut().process(&metric);
             if let Some(model) = res {
-                log_model = Some(model);
+                result = Some(model);
                 break;
             }
         }
 
-        if let Some(model) = log_model {
+        if let Some(inner) = result {
             for handler in &mut self.handler_list {
-                tokio::spawn(handler(model.clone()));
+                tokio::spawn(handler(inner.clone()));
             }
         }
     }
