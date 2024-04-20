f2e260717fa362afd0c2e940e10e74f1bd02834e
quannhg <quan.nguyenhg12@gmail.com>
2024-04-20 10:03:19 +0700

    feat(models): create LogModel as a return type for raw metrics processor

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -7,3 +7,9 @@ pub struct BrowserLogModel {
     pub start_time: String,
     pub title: Option<String>,
 }
+
+pub struct LogModel {
+    pub start_time: String,
+    pub end_time: String,
+    pub activity_identifier: String,
+}
