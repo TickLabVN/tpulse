10df5604b323ac21a4acc15d4428377655731464
HuyDNA <huydo862003@gmail.com>
2024-04-20 10:56:16 +0700

    feat: basic framework for raw metric processor

--- src-tauri/src/raw_metric_processor/mod.rs
+++ src-tauri/src/raw_metric_processor/mod.rs
@@ -1,1 +1,41 @@
+use std::{future::Future, pin::Pin};
 
+use crate::{metrics::UserMetric, models::LogModel};
+
+pub trait MetricProcessor {
+    fn process(&mut self, metric: &UserMetric) -> Option<LogModel>;
+}
+
+pub struct RawMetricProcessorManager {
+    processor_list: Vec<Box<dyn MetricProcessor>>,
+    handler_list: Vec<Box<dyn Fn(LogModel) -> Pin<Box<dyn Future<Output = ()> + Send + Sync>>>>,
+}
+
+impl RawMetricProcessorManager {
+    pub fn register_processor(&mut self, processor: impl MetricProcessor + 'static) {
+        self.processor_list.push(Box::new(processor));
+    }
+    pub fn register_handler(
+        &mut self,
+        handler: impl Fn(LogModel) -> Pin<Box<dyn Future<Output = ()> + Send + Sync>> + 'static,
+    ) {
+        self.handler_list.push(Box::new(handler));
+    }
+
+    pub async fn handle_metric(mut self: Pin<&mut Self>, metric: UserMetric) {
+        let mut log_model = None;
+        for processor in &mut self.processor_list {
+            let res = processor.as_mut().process(&metric);
+            if let Some(model) = res {
+                log_model = Some(model);
+                break;
+            }
+        }
+
+        if let Some(model) = log_model {
+            for handler in &mut self.handler_list {
+                tokio::spawn(handler(model.clone()));
+            }
+        }
+    }
+}
