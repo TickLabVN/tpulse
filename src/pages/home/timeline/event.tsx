390d4214d57a88e221a4e81c73268dbecd98bcf8
Minhtuei <tue.tran20032003@gmail.com>
2024-04-28 19:22:42 +0700

    fix(ui): fix textColor and textAlign

--- src/pages/home/timeline/event.tsx
+++ src/pages/home/timeline/event.tsx
@@ -11,7 +11,7 @@ interface EventProps {
 
 export function Event({ event, timeUnit, top }: EventProps) {
   const eventColor = {
-    backgroundColor: '#D0D7DE',
+    backgroundColor: '#DAE2EB',
     textColor: '#6E7781',
     borderColor: '#6E7781'
   };
@@ -41,11 +41,11 @@ export function Event({ event, timeUnit, top }: EventProps) {
   }, [timeUnit, start, end]);
   const firstContent = () => {
     return (
-      <div className='flex items-center gap-x-3'>
+      <div className={`flex items-center gap-x-3`}>
         <div
-          className={taskName ? 'text-[14px] font-[500] leading-[16.94px]' : 'text-xs font-normal leading-3'}
+          className={taskName ? 'text-[14px] font-[500] leading-[16.94px]' : `text-xs font-normal leading-3 `}
         >
-          {moment.unix(start).format('HH:mm')} - {moment.unix(end).format('HH:mm')}
+          {moment.unix(start).format('HH:mm A')} - {moment.unix(end).format('HH:mm A')}
         </div>
         {projectName ? (
           <div className='flex items-center px-3 py-1 gap-x-1'>
@@ -55,7 +55,7 @@ export function Event({ event, timeUnit, top }: EventProps) {
                 backgroundColor: eventColor.borderColor
               }}
             ></span>
-            <div className='text-xs font-normal leading-3'>{projectName}</div>
+            <div className='text-[14px] font-[500] leading-[16.94px]'>{projectName}</div>
           </div>
         ) : (
           <Badge className='!bg-green !text-xs !font-[500] !rounded-[5px] !px-1 !py-0.5 text-white !leading-3 !border-none ml-auto'>
@@ -68,10 +68,10 @@ export function Event({ event, timeUnit, top }: EventProps) {
   const secondContent = () => {
     return (
       <div className='flex items-center justify-between'>
-        <div className='flex items-center gap-x-1'>
-          {icon && <img src={icon} alt='vsc-icon' className='w-5 h-5 rounded-full' />}
-          <span className='text-xs font-semibold'>{title}</span>
-          {taskName && <span className='text-lg font-semibold leading-5'>{taskName}</span>}
+        <div className='flex items-center'>
+          {icon && <img src={icon} alt='vsc-icon' className='w-5 h-5 mr-1 rounded-full' />}
+          <span className={`text-xs font-semibold leading-[14.52px]`}>{title}</span>
+          {taskName && <span className='text-lg font-semibold leading-[21.78px]'>{taskName}</span>}
         </div>
         {taskName && (
           <Badge className='!bg-accent-blue !text-xs !font-[500] !rounded-[5px] !px-1 !py-0.5 text-white !leading-3 !border-none'>
@@ -89,7 +89,8 @@ export function Event({ event, timeUnit, top }: EventProps) {
         top: `${top}px`,
         height: `${height}px`,
         borderRadius: '10px',
-        zIndex: 1
+        zIndex: 1,
+        color: eventColor.textColor
       }}
     >
       <span
