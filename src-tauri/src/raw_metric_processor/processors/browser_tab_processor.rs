3883299e03ec8e6873055a4d2077467e762bd2aa
TanNguyen <duytan17112003@gmail.com>
2024-05-04 14:50:54 +0700

    feat(browser): change type of get_base from option to string

--- src-tauri/src/raw_metric_processor/processors/browser_tab_processor.rs
+++ src-tauri/src/raw_metric_processor/processors/browser_tab_processor.rs
@@ -6,14 +6,24 @@ pub struct BrowserTabProcessor;
 use std::error::Error;
 use url::Url;
 
-fn get_base_url(url: &str) -> Result<String, Box<dyn Error>> {
-    let parsed_url = Url::parse(url)?;
+// fn get_base_url(url: &str) -> Result<String, Box<dyn Error>> {
+//     let parsed_url = Url::parse(url)?;
+//     let base_url = format!(
+//         "{}://{}",
+//         parsed_url.scheme(),
+//         parsed_url.host_str().unwrap_or("")
+//     );
+//     Ok(base_url)
+// }
+
+fn get_base_url(url: &str) -> String {
+    let parsed_url = Url::parse(url).unwrap();
     let base_url = format!(
         "{}://{}",
         parsed_url.scheme(),
         parsed_url.host_str().unwrap_or("")
     );
-    Ok(base_url)
+    base_url
 }
 
 impl MetricProcessor for BrowserTabProcessor {
@@ -22,16 +32,9 @@ impl MetricProcessor for BrowserTabProcessor {
             UserMetric::Browser(browser_metric)
                 if browser_metric.data_type == BrowserMetricType::BrowserTab =>
             {
-                let url = match &browser_metric.url {
-                    Some(url) => match get_base_url(url) {
-                        Ok(base_url) => base_url,
-                        Err(_) => String::from("default"),
-                    },
-                    None => String::from("default"),
-                };
                 Some(StartActivity {
                     start_time: browser_metric.start_time as u64,
-                    activity_identifier: url,
+                    activity_identifier: get_base_url(&browser_metric.url.clone()?),
                 })
             }
             _ => None,
