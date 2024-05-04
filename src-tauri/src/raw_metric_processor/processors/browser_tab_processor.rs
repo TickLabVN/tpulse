05a83d08955da09214a78b91d384217d68bb9665
TanNguyen <duytan17112003@gmail.com>
2024-05-04 14:40:41 +0700

    feat(browser): get base url and refactor

--- src-tauri/src/raw_metric_processor/processors/browser_tab_processor.rs
+++ src-tauri/src/raw_metric_processor/processors/browser_tab_processor.rs
@@ -1,23 +1,38 @@
 use crate::metrics::{BrowserMetricType, UserMetric};
 use crate::raw_metric_processor::{MetricProcessor, StartActivity};
+use oauth2::url;
 
 pub struct BrowserTabProcessor;
+use std::error::Error;
+use url::Url;
+
+fn get_base_url(url: &str) -> Result<String, Box<dyn Error>> {
+    let parsed_url = Url::parse(url)?;
+    let base_url = format!(
+        "{}://{}",
+        parsed_url.scheme(),
+        parsed_url.host_str().unwrap_or("")
+    );
+    Ok(base_url)
+}
 
 impl MetricProcessor for BrowserTabProcessor {
     fn process(&mut self, metric: &UserMetric) -> Option<StartActivity> {
         match metric {
-            UserMetric::Browser(browser_metric) => {
-                if browser_metric.data_type == BrowserMetricType::BrowserTab {
-                    Some(StartActivity {
-                        start_time: browser_metric.start_time as u64,
-                        activity_identifier: browser_metric
-                            .url
-                            .clone()
-                            .unwrap_or_else(|| String::from("default")),
-                    })
-                } else {
-                    None
-                }
+            UserMetric::Browser(browser_metric)
+                if browser_metric.data_type == BrowserMetricType::BrowserTab =>
+            {
+                let url = match &browser_metric.url {
+                    Some(url) => match get_base_url(url) {
+                        Ok(base_url) => base_url,
+                        Err(_) => String::from("default"),
+                    },
+                    None => String::from("default"),
+                };
+                Some(StartActivity {
+                    start_time: browser_metric.start_time as u64,
+                    activity_identifier: url,
+                })
             }
             _ => None,
         }
