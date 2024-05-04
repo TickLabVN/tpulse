c3cd3805119e9082f38a50d5d010ffbe5058fcab
TanNguyen <duytan17112003@gmail.com>
2024-05-04 14:10:45 +0700

    feat(browser-processor): implement browser processor

--- src-tauri/src/raw_metric_processor/processors/browser_tab_processor.rs
+++ src-tauri/src/raw_metric_processor/processors/browser_tab_processor.rs
@@ -1,1 +1,25 @@
+use crate::metrics::{BrowserMetricType, UserMetric};
+use crate::raw_metric_processor::{MetricProcessor, StartActivity};
 
+pub struct BrowserTabProcessor;
+
+impl MetricProcessor for BrowserTabProcessor {
+    fn process(&mut self, metric: &UserMetric) -> Option<StartActivity> {
+        match metric {
+            UserMetric::Browser(browser_metric) => {
+                if browser_metric.data_type == BrowserMetricType::BrowserTab {
+                    Some(StartActivity {
+                        start_time: browser_metric.start_time as u64,
+                        activity_identifier: browser_metric
+                            .url
+                            .clone()
+                            .unwrap_or_else(|| String::from("default")),
+                    })
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+}
