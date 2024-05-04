d3e99b0b6fede1109f566a3203963ca8531edd18
Huy-DNA <huydo862003@gmail.com>
2024-05-04 16:40:44 +0700

    feat: init event_handler

--- src-tauri/src/event_handler/mod.rs
+++ src-tauri/src/event_handler/mod.rs
@@ -1,1 +1,7 @@
+use crate::raw_metric_processor::ProcessedResult;
+use std::{future::Future, pin::Pin};
+
 pub mod logger;
+
+pub type EventHandler =
+    Box<dyn Fn(Vec<ProcessedResult>) -> Pin<Box<dyn Future<Output = ()> + Send + Sync>>>;
