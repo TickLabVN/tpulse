cdfc3fc2bdf563e366c9a18b80ef0a926873f70f
Huy-DNA <huydo862003@gmail.com>
2024-05-25 11:51:36 +0700

    feat: register VSCodeProcessor

--- src-tauri/src/initializer/raw_metric_processor.rs
+++ src-tauri/src/initializer/raw_metric_processor.rs
@@ -2,7 +2,8 @@ use crate::{
     event_handler::{logger, make_event_handler},
     raw_metric_processor::{
         processors::{
-            browser_tab_processor::BrowserTabProcessor, youtube_processor::YoutubeProcessor,
+            browser_tab_processor::BrowserTabProcessor, vscode_processor::VSCodeProcessor,
+            youtube_processor::YoutubeProcessor,
         },
         RawMetricProcessorManager,
     },
@@ -14,6 +15,7 @@ pub fn initialize() -> RawMetricProcessorManager {
     // Register raw metric processors here
     raw_metric_processor.register_processor(YoutubeProcessor);
     raw_metric_processor.register_processor(BrowserTabProcessor);
+    raw_metric_processor.register_processor(VSCodeProcessor);
 
     // Register event handlers here
     raw_metric_processor.register_handler(make_event_handler(logger::handle_events));
