4b9afb409513a8693672f2e88a56d96df7ee9a91
Huy-DNA <huydo862003@gmail.com>
2024-05-25 12:24:05 +0700

    fix: WindowOS browser watcher signature + Linux browser watcher only sends data

--- src-tauri/src/watcher/browser/mod.rs
+++ src-tauri/src/watcher/browser/mod.rs
@@ -1,46 +1,24 @@
 mod utils;
 use crate::metrics::UserMetric;
-use crate::raw_metric_processor::processors::browser_tab_processor::BrowserTabProcessor;
-use crate::raw_metric_processor::MetricProcessor;
-use crate::raw_metric_processor::StartActivity;
 use std::sync::mpsc;
 
 use utils::{convert_to_user_metric, create_named_pipe, read_from_pipe};
 #[cfg(any(target_os = "linux", target = "macos"))]
 
-pub fn watch_browser(_tx: mpsc::Sender<UserMetric>) {
+pub fn watch_browser(tx: mpsc::Sender<UserMetric>) {
     let pipe_name = "/tmp/tpulse";
-    let mut processor: Box<dyn MetricProcessor> = Box::new(BrowserTabProcessor);
     match create_named_pipe(&pipe_name) {
         Ok(_) => println!("Creating named pipe successfully"),
         Err(err) => eprintln!("Error: {}", err),
     };
     loop {
-        match read_from_pipe(&pipe_name) {
-            Ok(data) => {
-                match convert_to_user_metric(data) {
-                    Ok(metric) => {
-                        let result = processor.process(&metric as &UserMetric);
-                        match result {
-                            Some(processed_result) => match processed_result {
-                                StartActivity {
-                                    start_time,
-                                    activity_identifier,
-                                    tag,
-                                } => {
-                                    println!(
-                                        "Start activity: {} at {}",
-                                        activity_identifier, start_time
-                                    );
-                                }
-                            },
-                            None => {
-                                println!("No processed result");
-                            }
-                        }
-                        // For handle insert into log
-                    }
-                    Err(err) => eprintln!("Failed to convert data to UserMetric: {}", err),
+        match read_from_pipe(&pipe_name)
+            .map_err(|e| e.to_string())
+            .and_then(|v| convert_to_user_metric(v).map_err(|e| e.to_string()))
+        {
+            Ok(metric) => {
+                if let Err(_) = tx.send(metric) {
+                    eprintln!("Failed to send browser metric");
                 }
             }
             Err(err) => eprintln!("Error: {}", err),
@@ -56,7 +34,7 @@ use {
 };
 
 #[cfg(target_os = "windows")]
-pub fn watch_browser() {
+pub fn watch_browser(tx: mpsc::Sender<UserMetric>) {
     let pipe_name = "\\\\.\\pipe\\tpulse";
     match create_named_pipe(&pipe_name) {
         Ok(pipe_handle) => {
@@ -65,11 +43,18 @@ pub fn watch_browser() {
                 let connected =
                     unsafe { ConnectNamedPipe(pipe_handle as *mut c_void, ptr::null_mut()) };
                 if connected == 0 {
-                    eprint!("Couldn't connect to named pipe")
+                    eprintln!("Couldn't connect to named pipe");
                 }
-                match read_from_pipe(pipe_handle) {
-                    Ok(data) => eprint!("Data from client: {}", data),
-                    Err(err) => eprint!("Failed to get data from client: {}", err),
+                match read_from_pipe(&pipe_name)
+                    .map_err(|e| e.to_string())
+                    .and_then(|v| convert_to_user_metric(v).map_err(|e| e.to_string()))
+                {
+                    Ok(metric) => {
+                        if let Err(_) = tx.send(metric) {
+                            eprintln!("Failed to send browser metric");
+                        }
+                    }
+                    Err(err) => eprintln!("Error: {}", err),
                 }
                 unsafe {
                     DisconnectNamedPipe(pipe_handle as *mut c_void);
