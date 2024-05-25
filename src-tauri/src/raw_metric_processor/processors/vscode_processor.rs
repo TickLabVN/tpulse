6960ad05108d3e6d0599982e7e9fceb003e1c192
Đỗ Nguyễn An Huy <huydo862003@gmail.com>
2024-05-25 10:58:47 +0700

    Update src-tauri/src/raw_metric_processor/processors/vscode_processor.rs

--- src-tauri/src/raw_metric_processor/processors/vscode_processor.rs
+++ src-tauri/src/raw_metric_processor/processors/vscode_processor.rs
@@ -41,7 +41,7 @@ fn extract_project_name_from_vscode_title(title: &str) -> Option<String> {
 
     let project_name = title_components.get(title_components.len() - 2);
 
-    return project_name.map(|name| name.to_string());
+    project_name.map(|name| name.to_string());
 }
 
 #[cfg(test)]
