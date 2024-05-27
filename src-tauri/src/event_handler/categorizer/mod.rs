452173fe82e45d4ca30aba5dfa78fc7bb8434584
Huy-DNA <huydo862003@gmail.com>
2024-05-27 19:59:26 +0700

    feat: basic logic for delegating to categorizer helpers

--- src-tauri/src/event_handler/categorizer/mod.rs
+++ src-tauri/src/event_handler/categorizer/mod.rs
@@ -1,8 +1,29 @@
-use crate::raw_metric_processor::ProcessedResult;
+use crate::raw_metric_processor::{ActivityTag, ProcessedResult, StartActivity};
+
+use self::helpers::inverted_index_categorizer;
 
 mod helpers;
 
 #[derive(Debug, PartialEq)]
 pub struct Category(String);
 
-pub fn handle_events(events: Vec<ProcessedResult>) {}
+pub fn handle_events(events: Vec<ProcessedResult>) {
+    events.into_iter().for_each(handle_event);
+}
+
+fn handle_event(event: ProcessedResult) {
+    if let StartActivity {
+        tag,
+        activity_identifier,
+        ..
+    } = event
+    {
+        let category = match tag {
+            ActivityTag::WINDOW | ActivityTag::BROWSER => {
+                inverted_index_categorizer::categorize(activity_identifier)
+            }
+            ActivityTag::VSCODE => None,
+            ActivityTag::YOUTUBE => None,
+        };
+    }
+}
