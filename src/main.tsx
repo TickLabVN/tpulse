da69cc992397f85a5d29c9ad857e9e0f024c6ee5
Nguyen Phuc Vinh <npvinh0507@gmail.com>
2024-07-08 20:05:59 +0700

    remove `react-toastify`

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import { attachConsole } from '@tauri-apps/plugin-log';
 import React from 'react';
 import 'react-circular-progressbar/dist/styles.css';
 import ReactDOM from 'react-dom/client';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import { routeTree } from './routeTree.gen';
 import { log } from './utils/log';
@@ -32,7 +30,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LocalizationProvider dateAdapter={AdapterMoment}>
       <QueryClientProvider client={queryClient}>
-        <ToastContainer limit={1} />
         <RouterProvider router={router} />
       </QueryClientProvider>
     </LocalizationProvider>
